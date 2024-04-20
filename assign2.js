e71ca39d7459d34ef33718a40f3d1d4177c74191
RefayaAftarin <refayaaftarin2018@gmail.com>
2024-04-20 22:37:32 +0600

    second commit

--- assign2.js
+++ assign2.js
@@ -1,11 +1,11 @@
 function connect(){
     var searchBox=document.getElementById("search").value;
     document.getElementById("search").value="";
-    url=`https://restcountries.com/v3.1/all?fields=${searchBox}` ;
+    url=`https://restcountries.com/v3.1/all?fields=name/${searchBox}?fullText=true` ;
 
     fetch(url)
     .then (res=>res.json())
-    .then(data=>show(`data.${searchBox}`))
+    .then(data=>show(data))
 
 }    
 function show (data) {
@@ -13,8 +13,8 @@ function show (data) {
      oldContent.textContent = ""; 
      for(var i=1; i<data.length; i++){
        var newDiv = document.createElement("div");
-       newDiv.innerHTML = `Common Name:${data[i-1].currencies}<br>;
-                           Official Name:${data[i-1].official}`<br><br> ; 
+       newDiv.innerHTML = `Common Name:${data[i-1].common}<br>;
+                           Official Name:${data[i-1].official}<br>`; 
        newDiv.classList.add("innerStyle");
        oldContent.appendChild(newDiv); 
      }
